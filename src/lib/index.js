17adb6d10192667c27ba4e512b10ba7e8519c98a
Hoang Thai Hoc <thaihoc06@gmail.com>
2021-04-09 16:41:26 +0700

    Fix bug export DraggingItem

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 import SimpleDragDrop from './components/SimpleDragDrop';
 import Droppable from './components/Droppable';
 import Draggable from './components/Draggable';
+import DraggingItem from './components/DraggingItem';
 
 export {
   SimpleDragDrop,
   Droppable,
   Draggable,
+  DraggingItem,
 };
